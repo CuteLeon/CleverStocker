fb577df5d283551f22b7bca76b522585520af87d
Leon <leon.id@qq.com>
2019-06-21 17:19:54 +0800

    添加项目文件。

---
+++ CleverStocker.Client/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CleverStocker.Client
+{
+    static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            using (var launcher = new LaunchForm())
+            {
+                switch (launcher.ShowDialog())
+                {
+                    case DialogResult.None:
+                    case DialogResult.OK:
+                    case DialogResult.Ignore:
+                    case DialogResult.Yes:
+                        {
+                            Application.Run(new MainForm());
+
+                            break;
+                        }
+                    case DialogResult.Cancel:
+                    case DialogResult.Abort:
+                    case DialogResult.Retry:
+                    case DialogResult.No:
+                        {
+                            Environment.Exit(-1);
+
+                            break;
+                        }
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
