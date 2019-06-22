2b0ab7942b0cc1c749cc5d9de5b351881862d932
Leon <Leon.ID@QQ.COM>
2019-06-22 12:42:32 +0800

    统一项目 Ruleset；增加爬虫项目，使用 HtmlAgilityPack 作为解析框架；

---
+++ CleverStocker.Spider/SpiderClient.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+
+namespace CleverStocker.Spider
+{
+    /// <summary>
+    /// 爬虫客户端
+    /// </summary>
+    public class SpiderClient
+    {
+        /// <summary>
+        /// 网络客户端
+        /// </summary>
+        public HtmlWeb WebClient = null;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SpiderClient"/> class.
+        /// </summary>
+        public SpiderClient()
+        {
+            this.WebClient = new HtmlWeb()
+            {
+            };
+        }
+    }
+}
---
+++ CleverStocker.Spider/SpiderClientHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleverStocker.Spider
+{
+    /// <summary>
+    /// 爬虫客户端助手
+    /// </summary>
+    public class SpiderClientHelper
+    {
+    }
+}
