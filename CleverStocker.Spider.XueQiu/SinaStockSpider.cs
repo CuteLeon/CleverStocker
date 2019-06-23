c07486fad4f85fb126ebe087110fd03f79fad52d
Leon <Leon.ID@QQ.COM>
2019-06-23 11:09:49 +0800

    增加新浪股票爬虫项目；

---
+++ CleverStocker.Spider.XueQiu/SinaStockSpider.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using CleverStocker.Model;
+using CleverStocker.Services;
+using CleverStocker.Spider.HtmlAgilityPack;
+
+namespace CleverStocker.Spider.XueQiu
+{
+    /// <summary>
+    /// 新浪股票爬虫
+    /// </summary>
+    public class SinaStockSpider : HAPSpiderClient, IStockerService
+    {
+        /// <summary>
+        /// 获取股票集合
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<Stock> GetStocks()
+        {
+            return Enumerable.Range(0, 10).Select(index => new Stock());
+        }
+    }
+}
