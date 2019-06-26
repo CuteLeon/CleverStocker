0890b760cccb0a51612cd2bf3fefeec2080b6f15
Leon <leon.id@qq.com>
2019-06-26 14:01:27 +0800

    增加Coomon项目；

---
+++ CleverStocker.Common/CommonStandard.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace CleverStocker.Common
+{
+    /// <summary>
+    /// 通用标准
+    /// </summary>
+    public static class CommonStandard
+    {
+        /// <summary>
+        /// 交易市场
+        /// </summary>
+        public enum Markets
+        {
+            /// <summary>
+            /// 上交所
+            /// </summary>
+            ShangHai = 0,
+
+            /// <summary>
+            /// 深交所
+            /// </summary>
+            ShenZhen = 1,
+        }
+
+        /// <summary>
+        /// Gets 交易市场代码和名称字典
+        /// </summary>
+        public static ReadOnlyDictionary<Markets, (string Code, string Name)> MarketDictionary { get; } =
+            new ReadOnlyDictionary<Markets, (string Code, string Name)>(
+                new Dictionary<Markets, (string Code, string Name)>()
+                {
+                    { Markets.ShangHai, ("sh", "上交所") },
+                    { Markets.ShangHai, ("sz", "深交所") },
+                });
+    }
+}
