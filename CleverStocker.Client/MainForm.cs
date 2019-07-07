a39a18fe7bbe176ea95bd98b474e672b8039edbb
Leon <Leon.ID@QQ.COM>
2019-07-07 17:58:59 +0800

    fixed;

--- CleverStocker.Client/MainForm.cs
+++ CleverStocker.Client/MainForm.cs
@@ -15,12 +15,9 @@ using static CleverStocker.Common.CommonStandard;
 namespace CleverStocker.Client
 {
     /* <功能布局：>
-     * 自选股票
+     * 线图x4
      *
-     * 文档 (线图x4)
-     *
-     * 实时行情 (3s)
-     * 大盘指数
+     *大盘指数
      * 公司信息
      * 最近交易(列表x4)
      * 最近行情(时间规模x5)
