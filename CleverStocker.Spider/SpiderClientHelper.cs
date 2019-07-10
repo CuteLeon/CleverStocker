4a8b1a08a8c4c2af1dbda2bbcf6d04cc1f958245
Leon <leon.id@qq.com>
2019-07-10 17:46:09 +0800

    分析：新浪财经爬虫-使用 代码\名称\拼音 搜索股票；

--- CleverStocker.Spider/SpiderClientHelper.cs
+++ CleverStocker.Spider/SpiderClientHelper.cs
@@ -6,6 +6,7 @@ namespace CleverStocker.Spider
 {
     // TODO: 腾讯财经爬虫-实时资金流向 (http://qt.gtimg.cn/q=sz000858)
     // TODO: 网易财经爬虫-历史所有交易日行情 (csv 格式)
+    // TODO: 新浪财经爬虫-使用 代码\名称\拼音 搜索股票
 
     /// <summary>
     /// 爬虫客户端助手
