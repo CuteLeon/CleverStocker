4a834a84f4b80a399ff6588e4844d9ee0653433c
Leon <leon.id@qq.com>
2019-07-10 18:09:44 +0800

    实现序列化助手；

--- CleverStocker.Utils/SerializerHelper.cs
+++ CleverStocker.Utils/SerializerHelper.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.IO;
 using System.Text;
-using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace CleverStocker.Utils
 {
@@ -11,6 +9,57 @@ namespace CleverStocker.Utils
     /// </summary>
     public static class SerializerHelper
     {
-        // TODO: 使用极速序列化助手帮助 MQ 传输对象数据，减轻每次从 DbContext Find() 的压力；
+        /// <summary>
+        /// 序列化
+        /// </summary>
+        /// <typeparam name="T">对象类型</typeparam>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        public static string Serialize<T>(this T source)
+            where T : class, new()
+        {
+            try
+            {
+                var serializer = new XmlSerializer(typeof(T));
+                using (MemoryStream stream = new MemoryStream())
+                {
+                    using (var writer = new StreamWriter(stream, Encoding.UTF8))
+                    {
+                        serializer.Serialize(writer, source);
+                        return Encoding.UTF8.GetString(stream.GetBuffer());
+                    }
+                }
+            }
+            catch
+            {
+                return default;
+            }
+        }
+
+        /// <summary>
+        /// 反序列化
+        /// </summary>
+        /// <typeparam name="T">对象类型</typeparam>
+        /// <param name="content">内容</param>
+        /// <returns>对象</returns>
+        public static T Deserialize<T>(this string content)
+            where T : class, new()
+        {
+            try
+            {
+                var serializer = new XmlSerializer(typeof(T));
+                using (MemoryStream stream = new MemoryStream(Encoding.UTF8.GetBytes(content)))
+                {
+                    using (StreamReader reader = new StreamReader(stream, Encoding.UTF8))
+                    {
+                        return serializer.Deserialize(reader) as T;
+                    }
+                }
+            }
+            catch
+            {
+                return default;
+            }
+        }
     }
 }
---
+++ CleverStocker.UtilsTests/SerializerHelperTests.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CleverStocker.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CleverStocker.Model;
+using static CleverStocker.Common.CommonStandard;
+
+namespace CleverStocker.Utils.Tests
+{
+    [TestClass()]
+    public class SerializerHelperTests
+    {
+        [TestMethod()]
+        public void SerializerHelperTest()
+        {
+            Quota quota = new Quota()
+            {
+                Amount = 100_0000,
+                AuctionPrice = 110,
+                BiddingPrice = 90,
+                BuyPrice1 = 91,
+                BuyPrice2 = 92,
+                BuyPrice3 = 93,
+                BuyPrice4 = 94,
+                BuyPrice5 = 95,
+                BuyStrand1 = 1000,
+                BuyStrand2 = 2000,
+                BuyStrand3 = 3000,
+                BuyStrand4 = 4000,
+                BuyStrand5 = 5000,
+                ClosingPriceYesterday = 95,
+                Code = "000001",
+                Count = 10000,
+                CurrentPrice = 100,
+                DayHighPrice = 110,
+                DayLowPrice = 90,
+                Market = Markets.ShangHai,
+                Name = "上证指数",
+                OpeningPriceToday = 105,
+                SellPrice1 = 115,
+                SellPrice2 = 114,
+                SellPrice3 = 113,
+                SellPrice4 = 112,
+                SellPrice5 = 111,
+                SellStrand1 = 1000,
+                SellStrand2 = 2000,
+                SellStrand3 = 3000,
+                SellStrand4 = 4000,
+                SellStrand5 = 5000,
+                Stock = new Stock("000001", Markets.ShangHai, "上证指数"),
+                UpdateTime = new DateTime(2020, 12, 31, 12, 30, 15),
+            };
+
+            // 将导致循环引用问题
+            // quota.Stock.Quotas.Add(quota);
+
+            var content = SerializerHelper.Serialize(quota);
+            Assert.IsFalse(string.IsNullOrWhiteSpace(content));
+
+            var newQuota = SerializerHelper.Deserialize<Quota>(content);
+
+            // TODO: 需要验证数据并测试性能
+        }
+    }
+}
