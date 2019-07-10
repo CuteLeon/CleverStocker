01483d66cb1f23923db29cf71c48da350aae9ef7
Leon <leon.id@qq.com>
2019-07-10 11:46:24 +0800

    SerializerHelper TODO;

---
+++ CleverStocker.Utils/SerializerHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleverStocker.Utils
+{
+    /// <summary>
+    /// 序列化助手
+    /// </summary>
+    public static class SerializerHelper
+    {
+        // TODO: 使用极速序列化助手帮助 MQ 传输对象数据，减轻每次从 DbContext Find() 的压力；
+    }
+}
